c6232076de3fdf5175aa3995e22a4feabeecfa13
jNizM <jNizM@outlook.com>
2023-10-24 11:28:13 +0200

    Add AdobeAcrobatDC JavaScript remediation
    Disable JavaScript on Adobe Acrobat DC

---
+++ Remediations/Disable_AdobeAcrobatDC_JavaScript/AdobeAcrobatDC_JavaScript-Remediation.ps1
@@ -0,0 +1,20 @@
+$RegPath  = 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown'
+$RegName  = 'bDisableJavaScript'
+$RegValue = 1
+$RegType  = 'DWord'
+
+
+if ((Test-Path -LiteralPath $RegPath) -ne $true)
+{
+	New-Item $RegPath -force -ea SilentlyContinue
+}
+
+try
+{
+	New-ItemProperty -LiteralPath $RegPath -Name $RegName -Value $RegValue -PropertyType $RegType -Force -ea SilentlyContinue
+}
+catch
+{
+	Exit 1
+}
+Exit 0
